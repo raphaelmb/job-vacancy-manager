8cd6ea3303e21254b0db5819ab02e9458a76aeed
Raphael Berger <raphaelmb@gmail.com>
2025-04-01 13:33:52 -0300

    new: job controller refactor

--- src/main/java/br/com/raphaelmb/job_vacancy_manager/modules/company/controllers/JobController.java
+++ src/main/java/br/com/raphaelmb/job_vacancy_manager/modules/company/controllers/JobController.java
@@ -1,5 +1,7 @@
 package br.com.raphaelmb.job_vacancy_manager.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.raphaelmb.job_vacancy_manager.modules.company.dto.CreateJobDTO;
 import br.com.raphaelmb.job_vacancy_manager.modules.company.entities.JobEntity;
 import br.com.raphaelmb.job_vacancy_manager.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,7 +22,16 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id");
+
+        var jobEntity = JobEntity.builder()
+            .benefits(createJobDTO.getBenefits())
+            .description(createJobDTO.getDescripton())
+            .level(createJobDTO.getLevel())
+            .companyId(UUID.fromString(companyId.toString()))
+            .build();
+
         var result = this.createJobUseCase.execute(jobEntity);
         return ResponseEntity.status(201).body(result);
     }
---
+++ src/main/java/br/com/raphaelmb/job_vacancy_manager/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,10 @@
+package br.com.raphaelmb.job_vacancy_manager.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+    private String descripton;    
+    private String benefits;    
+    private String level;    
+}
